a790fcffad606c2fe4bc76a4fb8266f6ad50b5b6
Kaspar Sunekær <git@sunekaer.com>
2026-03-28 20:53:14 +0100

    feat: port to 26.1

--- neoforge/src/main/java/com/sunekaer/yeetusexperimentus/neo/YeetusExperimentusNeoForge.java
+++ neoforge/src/main/java/com/sunekaer/yeetusexperimentus/neo/YeetusExperimentusNeoForge.java
@@ -1,11 +1,13 @@
 package com.sunekaer.yeetusexperimentus.neo;
 
 import com.sunekaer.yeetusexperimentus.YeetusExperimentus;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(YeetusExperimentus.MOD_ID)
 public class YeetusExperimentusNeoForge {
-    public YeetusExperimentusNeoForge() {
+    public YeetusExperimentusNeoForge(IEventBus modEventBus, ModContainer modContainer) {
         YeetusExperimentus.init();
     }
 }
